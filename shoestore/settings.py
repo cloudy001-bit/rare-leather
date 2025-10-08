46b5014247427ba0510e2096d95cc20d8f83d225
Cloudy <faboyawals6@gmail.com>
2025-10-08 12:41:29 +0100

    Install Whitenoise and fix static middleware

--- shoestore/settings.py
+++ shoestore/settings.py
@@ -115,6 +115,7 @@ TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
