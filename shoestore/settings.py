9baa81899ce44a7e1622bc5f922530d08a03150e
Cloudy <faboyawals6@gmail.com>
2025-10-09 07:57:22 +0100

    EMAIL CONFIG HARDCODED REVERT TRY FIX 1 REVERT

--- shoestore/settings.py
+++ shoestore/settings.py
@@ -43,9 +43,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or "noreply@rareleather.com"
+EMAIL_HOST_USER = "omanmannsh@gmail.com"
+EMAIL_HOST_PASSWORD = "jzqp ieui iuws qdan"
+DEFAULT_FROM_EMAIL = f"Rare Leather <{EMAIL_HOST_USER}>"
 
 # PAYSTACK
 PAYSTACK_PUBLIC_KEY = os.environ.get("PAYSTACK_PUBLIC_KEY")
